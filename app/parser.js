ee65ef638c999f0f92ffa8becdf86c5954b32136
Yonux <yonux@arcor.de>
2022-05-10 00:17:16 +0200

    updated parser.js

--- app/parser.js
+++ app/parser.js
@@ -4,35 +4,92 @@ const { JSDOM } = require('jsdom');
 function parse(url) {
     console.log(url)
     
-    axios.get(url)
-    .then(html => {
-        console.log(html);
+    axios.get(url).then(html => {
+
         const dom = new JSDOM(html.data);
         const document = dom.window.document;
 
-        const tablePlan = document.querySelector("tbody");
-        const rows = tablePlan.childNodes;
+        const tablePlan = document.querySelector("table");
+        tablePlan.setAttribute("id", "splan-table");
+        const rows = document.querySelectorAll("#splan-table > tbody > tr");
         
+        let planTitle = "benutzerdefiniert"; 
         const timeSlots = [];
-        const rowHeader = tablePlan.firstChild;
-        rowHeader.childNodes.forEach(header => {
-            const bounds = header.innerHtml.split(" - ");
-            if (bounds.length != 2) return;
-
-            timeSlots.push({
-                start:  bounds[0],
-                end:    bounds[1]
-            });
+        const rowHeader = rows[0];
+        rowHeader.querySelectorAll("td").forEach((header, idx) => {
+            if (idx == 0) {
+                planTitle = header.textContent;
+            } else {
+                const bounds = header.innerHTML.split(" - ");
+                if (bounds.length != 2) return;
+    
+                timeSlots[idx] = {
+                    "start":  bounds[0],
+                    "end":    bounds[1]
+                };
+            }
         });
+        console.log(timeSlots);
 
-        const events = [];
+        const courses = [];
+        const tdCourses = [...rows].slice(1);
+        let day = 0;
 
-        const trDays = [...rows].slice(1);
-        for (let day of trDays) {
-            if (day.rowSpan > 1) {
 
+        for (let row = 0; row < tdCourses.length; row++) {
+            const tr = tdCourses[row];
+            for (let col = 0; col < tdCourses[row].childElementCount; col++) {
+                let elem = [...tr.children][col];
+
+                if (col == 0) {
+                    // found field for name of weekday, start new day
+                    day++;
+                } else {
+                    const length = elem.colspan ?? 1;
+                    if (elem.childElementCount > 0) {
+                        console.log(elem.innerHTML)
+                        const courseName = elem.querySelector(".splan_veranstaltung").innerHTML.replaceAll("&nbsp;", " ").trim();
+
+                        const employeeAttrs = [...elem.querySelectorAll(".splan_mitarbeiter a")];
+                        const employeeObjs = employeeAttrs.map(empl => {
+                            const emplNameParts = empl.title.split(", ");
+                            if (emplNameParts.length != 2) return undefined;
+
+                            return {
+                                "first_name": emplNameParts[1],
+                                "last_name": emplNameParts[0],
+                                "abbr": empl.textContent
+                            };
+                        });
+
+                        const roomAttrs = [...elem.querySelectorAll(".splan_hoerer_raum a")];
+                        const roomObjs = roomAttrs.map(room => {
+                            return {
+                                "name": room.title,
+                                "abbr": room.textContent
+                            };
+                        });
+
+                        courses.push({
+                            "name": courseName,
+                            "time": {
+                                "day": day,
+                                "start": timeSlots[col].start,
+                                "end": timeSlots[col + length - 1].end,
+                                "length": length
+                            },
+                            "employees": employeeObjs,
+                            "rooms": roomObjs
+                        });
+                    }
+                }
             }
         }
+
+        return {
+            "planTitle": planTitle,
+            "courses": courses
+        };
 
     });
 }
